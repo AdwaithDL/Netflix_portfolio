555c0cfeaa5e481a6bb5dbd5e66b31cf08bca928
ADWAITH DL <126150192+AdwaithDL@users.noreply.github.com>
2026-09-08 00:14:06 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -115,7 +115,7 @@ const About = () => {
                 <span className="text-white font-bold drop-shadow">
                   Adwaith DL
                 </span>
-                , Security Analyst focused on SOC operations, threat
+                , a Security Analyst focused on SOC operations, threat
                 detection, and cloud security.
               </p>
 
